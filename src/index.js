7965c1bb654073dff974fa1b11caab7bb35087f3
Kristine Kochie <kristinekochie@gmail.com>
2022-06-14 22:56:55 -0400

    Core deliverables met

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  const form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    const li = document.createElement('li')
+    const p = document.createElement('p')
+    const btn = document.createElement('button')
+
+    p.textContent = e.target["new-task-description"].value + " "
+    btn.textContent = " x "
+    btn.addEventListener('click',()=>li.remove())
+
+    p.appendChild(btn)
+    li.append(p)
+    document.querySelector('#tasks').append(li)
+  })
+
+})
